435fc38393533e5f88eaea396c36724d6b8c3075
annecha562 <annecha562@gmail.com>
2018-09-14 22:19:01 +0600

    Added tests

--- src/ru/imit/september/Main.java
+++ src/ru/imit/september/Main.java
@@ -7,11 +7,6 @@ public class Main {
         a.doubleDigits();
         a.intDigits();
         a.rootSearch();
-        a.tabulation(1,6,0.5);
-        a.solutionSystemEquat(0,0,0,8,8,0);
-        System.out.println(a.decompose(2,0.6));
-        //ArrayWork b= new ArrayWork();
-        //int mass[]={2,5,4,6,0,-2};
-        //b.outputToConsole(b.keyboardInput(5));
+
     }
 }
